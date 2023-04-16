d479f6d1b9fdc1b07d3de7e6b99f9194902c975e
juli_ko <yuliakolomiets09@gmail.com>
2023-04-17 00:34:10 +0300

    5

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let arr = str.split("");
+  let result = "";
+  let count = 1;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === arr[i - 1]) {
+      count += 1;
+    } else {
+      count === 1
+        ? (result += arr[i - 1])
+        : (result += `${count}${arr[i - 1]}`);
+      count = 1;
+    }
+    if (i === arr.length - 1) {
+      count === 1 ? (result += arr[i]) : (result += `${count}${arr[i]}`);
+    }
+  }
+  return result;
 }
 
 module.exports = {
--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  let arr = n.split("-"); 
+  let arr2 = n.split('');
+  let arr3 =['-','0','1','2','3','4','5','6','8','9','A','B','C','D','E','F'];
+  if (arr.every((elem)=>elem.length===2)){
+    if (arr2.every((elem)=>arr3.includes(elem))){
+      return true
+    }
+  }
+  return false
 }
 module.exports = {
   isMAC48Address
